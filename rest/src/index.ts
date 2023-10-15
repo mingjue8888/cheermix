b95fbb5cff42d699195a3c8417e5588804e557e7
juessky <juessky@aliyun.com>
2023-10-15 23:38:03 +0800

    bug fix

--- rest/src/index.ts
+++ rest/src/index.ts
@@ -174,7 +174,7 @@ function printError(error: Error, _reqeust: Request, _response: Response, next: 
 export const hasAuthentication = passport.authenticate("jwt", { session: false })
 
 interface StartupOptions {
-  authenticationFindUserLogic?: <T>(payload: JwtPayload) => Promise<T>
+  authenticationFindUserLogic?: (payload: JwtPayload) => Promise<unknown>
   middlewaresExtension?: Middleware[]
 }
 
