842024992e5a38ed6e588f2e40e96c42e3c94317
juessky <juessky@aliyun.com>
2023-10-23 11:20:34 +0800

    delete env

--- logger/src/index.ts
+++ logger/src/index.ts
@@ -1,6 +1,28 @@
+import joi from "joi"
 import dayjs from "@cheermix/timeutil"
 import winston from "winston"
-import { LOGGER_COLORIZE, LOGGER_LEVEL, LOGGER_TEXT_ALIGN, LOGGER_TIME_FORMAT } from "./environment"
+
+interface ProcessEnvironment {
+  LOGGER_LEVEL: "error" | "warn" | "info" | "http" | "debug"
+  LOGGER_COLORIZE: boolean
+  LOGGER_TEXT_ALIGN: boolean
+  LOGGER_TIME_FORMAT: string
+}
+
+export const { LOGGER_LEVEL, LOGGER_COLORIZE, LOGGER_TEXT_ALIGN, LOGGER_TIME_FORMAT } = joi.attempt<
+  joi.ObjectSchema<ProcessEnvironment>
+>(
+  process.env,
+  joi.object({
+    LOGGER_LEVEL: joi.string().allow("error", "warn", "info", "http", "debug").default("debug"),
+    LOGGER_COLORIZE: joi.boolean().default(true),
+    LOGGER_TEXT_ALIGN: joi.boolean().default(true),
+    LOGGER_TIME_FORMAT: joi.string().default("YYYY-MM-DD HH:mm:ss.SSS"),
+  }),
+  {
+    allowUnknown: true,
+  }
+)
 
 const levels = {
   error: 0,
