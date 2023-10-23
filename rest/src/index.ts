78ea99d2a3884a9d44446a352c933db954376afe
juessky <juessky@aliyun.com>
2023-10-23 11:25:10 +0800

    delete env

--- rest/src/index.ts
+++ rest/src/index.ts
@@ -20,17 +20,52 @@ import rateLimit from "express-rate-limit"
 import morgan from "morgan"
 import cors from "cors"
 import compression from "compression"
-import {
-  EXPRESS_REQUEST_LIMIT_MAX,
-  EXPRESS_REQUEST_LIMIT_TIMEFRAME,
-  JWT_EXPIRES,
-  JWT_REFRESH_TIME,
-  JWT_SECRET,
-  NODE_PORT,
-} from "./environment"
 import logger from "@cheermix/logger"
 import { getUnixTimestamp } from "@cheermix/timeutil"
 import { compareSync, genSaltSync, hashSync } from "bcrypt"
+
+interface ProcessEnvironment {
+  NODE_PORT: number
+
+  EXPRESS_REQUEST_LIMIT_TIMEFRAME: number
+  EXPRESS_REQUEST_LIMIT_MAX: number
+
+  JWT_SECRET: string
+  JWT_EXPIRES: number
+  JWT_REFRESH_TIME: number
+}
+
+export const {
+  NODE_PORT,
+
+  EXPRESS_REQUEST_LIMIT_TIMEFRAME,
+  EXPRESS_REQUEST_LIMIT_MAX,
+
+  JWT_SECRET,
+  JWT_EXPIRES,
+  JWT_REFRESH_TIME,
+} = joi.attempt<joi.ObjectSchema<ProcessEnvironment>>(
+  process.env,
+  joi.object({
+    NODE_PORT: joi.number().integer().default(80),
+
+    EXPRESS_REQUEST_LIMIT_TIMEFRAME: joi.number().integer().default(1000),
+    EXPRESS_REQUEST_LIMIT_MAX: joi.number().integer().default(20),
+
+    JWT_SECRET: joi.string().default("hello world!"),
+    JWT_EXPIRES: joi
+      .number()
+      .integer()
+      .default(1000 * 60 * 60 * 48),
+    JWT_REFRESH_TIME: joi
+      .number()
+      .integer()
+      .default(1000 * 60 * 60),
+  }),
+  {
+    allowUnknown: true,
+  }
+)
 
 type ResponseJoiData = unknown
 type RequestJoiData = {
