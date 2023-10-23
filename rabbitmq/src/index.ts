6c4908e98411ab13b2ac314dafc5178538a6c220
juessky <juessky@aliyun.com>
2023-10-23 11:28:27 +0800

    delete env

--- rabbitmq/src/index.ts
+++ rabbitmq/src/index.ts
@@ -1,8 +1,22 @@
 import amqp from "amqplib"
-import { MQ_PREFETCH, MQ_URL } from "./environment"
-import { Observable, Subscriber, bufferTime, filter, map } from "rxjs"
+import { Observable, Subscriber, filter, map } from "rxjs"
 import _ from "lodash"
 import joi from "joi"
+interface ProcessEnvironment {
+  MQ_URL: string
+  MQ_PREFETCH: number
+}
+
+export const { MQ_URL, MQ_PREFETCH } = joi.attempt<joi.ObjectSchema<ProcessEnvironment>>(
+  process.env,
+  joi.object({
+    MQ_URL: joi.string(),
+    MQ_PREFETCH: joi.number().integer().greater(0).default(1),
+  }),
+  {
+    allowUnknown: true,
+  }
+)
 
 export async function getAliveChannel(): Promise<amqp.Channel> {
   return amqp
